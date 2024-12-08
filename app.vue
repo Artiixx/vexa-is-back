6ab70038686f2805a8206123b8bfa0518ced64b4
Artix <maxime@vexastudio.com>
2024-12-07 19:35:46 -0500

    🧨 Files upload

---
+++ app.vue
@@ -0,0 +1,12 @@
+<template>
+ <div class="min-h-screen bg-cover bg-center bg-[url('https://iili.io/2Gi4y3G.png')]">
+<div class="flex h-screen">
+  <div class="m-auto">
+    <img class="h-auto max-w-full hover:skew-y-6 transition" src="/public/assets/logo.svg" alt="image description">
+    <h1 class="text-center text-5xl text-white font-bold font-['Montserrat'] m-auto transition duration-700 hover:scale-125">Vexa 2025</h1>
+    <h1 class="italic hover:underline text-center text-2xl font-bold font-['Montserrat'] m-auto text-[#06f47c]">#StayTuned</h1>
+  </div>
+</div>
+</div>
+
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,5 @@
+export default defineNuxtConfig({
+  modules: ['@nuxt/fonts', '@nuxtjs/tailwindcss'],
+  compatibilityDate: '2024-11-01',
+  devtools: { enabled: true },
+})
